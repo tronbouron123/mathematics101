8bae7f8e80176df7aaf0678e2537cf34195b143e
tronbouron123 <tronbouron123@gmail.com>
2025-03-12 05:39:18 +0000

    Add 7.cpp

---
+++ src/7.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // This is a comment in C++
+    cout << "Hello, World!" << endl; // print Hello, World! to the console
+    return 0;
+}
