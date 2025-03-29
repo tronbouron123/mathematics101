3a5556c5f5e61ba9d2f1afa664caf166081e99e9
tronbouron123 <tronbouron123@gmail.com>
2025-03-29 20:07:31 +0000

    Add 19.cpp

---
+++ src/19.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    if (x > y) {
+        cout << "x is greater than y." << endl;
+    } else {
+        cout << "y is greater than x." << endl;
+    }
+    return 0;
+}
