4836f7f59dbbb71c9df0bd27960de970d0ced73f
tronbouron123 <tronbouron123@gmail.com>
2025-03-30 22:56:33 +0000

    Add 20.cpp

---
+++ src/20.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 3;
+    float z = 8.5f;
+    
+    cout << "The value of x + y is: " << (x + y) << endl;
+    cout << "The value of z squared is: " << pow(z, 2) << endl;
+
+    return 0;
+}
