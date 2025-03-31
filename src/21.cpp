64520095d5ba1d2ca2c163f73bbebce51370ffaf
tronbouron123 <tronbouron123@gmail.com>
2025-03-31 16:20:56 +0000

    Add 21.cpp

---
+++ src/21.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int x = 5;
+    if (x > 10) {
+        std::cout << "The value of x is greater than 10." << std::endl;
+    } else {
+        std::cout << "The value of x is not greater than 10." << std::endl;
+    }
+    return 0;
+}
