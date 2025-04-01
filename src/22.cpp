68f833a36c03930d5a4a02b9e3ad19cf915ea2eb
tronbouron123 <tronbouron123@gmail.com>
2025-04-01 15:14:16 +0000

    Add 22.cpp

---
+++ src/22.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    int y = 10;
+    cout << "The sum of " << x << " and " << y << " is: " << (x + y) << endl;
+    return 0;
+}
