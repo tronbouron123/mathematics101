fcaf15ddc094daa6ce77e8bdb5cb4827bdaec496
tronbouron123 <tronbouron123@gmail.com>
2025-04-03 12:57:06 +0000

    Add 23.cpp

---
+++ src/23.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 5;
+    if (num > 0) {
+        cout << "The number is positive." << endl;
+    } else {
+        cout << "The number is not positive." << endl;
+    }
+    return 0;
+}
