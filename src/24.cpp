935614b3f2b6d738ee9255a20e278dcdb0af0536
tronbouron123 <tronbouron123@gmail.com>
2025-04-05 05:16:21 +0000

    Add 24.cpp

---
+++ src/24.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 3;
+    
+    if (a > b) {
+        cout << "The greater number is: " << a << endl;
+    } else {
+        cout << "The greater number is: " << b << endl;
+    }
+    
+    return 0;
+}
