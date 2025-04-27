43ccf56edd36837596c192a10ec46572bdf23ea4
tronbouron123 <tronbouron123@gmail.com>
2025-04-27 08:20:17 +0000

    Add 36.cpp

---
+++ src/36.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+    
+    if (num1 > num2) {
+        cout << "The first number is greater than the second number." << endl;
+    } else {
+        cout << "The second number is greater than the first number." << endl;
+    }
+    
+    return 0;
+}
