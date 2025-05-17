10415e7ed1a037da79a11aecb34efe46a792f8a4
tronbouron123 <tronbouron123@gmail.com>
2025-05-17 06:43:30 +0000

    Add 39.cpp

---
+++ src/39.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.14;
+    
+    cout << "x is " << x << endl;
+    cout << "y is " << y << endl;
+    
+    return 0;
+}
