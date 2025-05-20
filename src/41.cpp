254cb81511dd499403af320423bd44dec5d67083
tronbouron123 <tronbouron123@gmail.com>
2025-05-20 00:25:43 +0000

    Add 41.cpp

---
+++ src/41.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5;
+    double y = 3.1415926536;
+    char z = 'A';
+
+    cout << "x is " << x << endl;
+    cout << "y is " << y << endl;
+    cout << "z is " << z << endl;
+
+    return 0;
+}
