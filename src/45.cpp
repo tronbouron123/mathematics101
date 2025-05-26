37e83295ade629534d915342bd9ebe11167fd22c
tronbouron123 <tronbouron123@gmail.com>
2025-05-26 04:03:54 +0000

    Add 45.cpp

---
+++ src/45.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    int sum = num1 + num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum << endl;
+    return 0;
+}
